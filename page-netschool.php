af3356613b1ceeff0cbd35fc2dbba4edf3ac79f4
Alexander Drozdov <snosme@gmail.com>
2017-07-15 01:52:18 +0300

    Update page-netschool.php

--- page-netschool.php
+++ page-netschool.php
@@ -1,9 +1,5 @@
 <?php get_header(); ?>
 
-<?php
-$url = ($_SERVER['REMOTE_ADDR'] == '178.151.205.106') ? 'http://192.168.0.52/login.asp' : 'http://178.151.205.106:70/login.asp';
-?>
-
-<iframe src="<?php echo $url; ?>" width="100%" height="100%"></iframe>
+<iframe src="https://netschool.academlyceum.zp.ua/login.asp" width="100%" height="100%"></iframe>
 
 <?php get_footer(); ?>
